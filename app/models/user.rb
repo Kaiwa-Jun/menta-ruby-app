cba98b89fc9f0464e6f37690babdc88b91170f9a
Kaiwa-Jun <kj.11235813213455@gmail.com>
2024-08-15 01:20:35 +0900

    step11完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :posts
+
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
