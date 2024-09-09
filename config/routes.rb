2fff2c9dea90e354ec9a4652eeb5caaef074d987
2litrewt <buicnuwe0170@gmail.com>
2024-09-09 20:09:34 +0900

    step18/user-follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'posts#index'
   delete 'logout'  => 'sessions#destroy', as: :logout
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  	get "followings" => "relationships#followings", as: "followings"
+  	get "followers" => "relationships#followers", as: "followers"
+  end
+  
   resources :sessions, only: [:new, :create]
   resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
